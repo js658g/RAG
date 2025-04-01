fd4b7c0570d462b41bac6e22ff66c57aaff65429
Jay <surenderjamplu@gmail.com>
2025-04-01 02:35:41 +0000

    custom task using langchain

---
+++ langchain-custom-task.py
@@ -0,0 +1,28 @@
+from langchain_core.callbacks.manager import (
+    adispatch_custom_event,
+)
+from langchain_core.runnables import RunnableLambda, RunnableConfig
+import asyncio
+
+
+async def slow_thing(some_input: str, config: RunnableConfig) -> str:
+    """Do something that takes a long time."""
+    await asyncio.sleep(1) # Placeholder for some slow operation
+    await adispatch_custom_event(
+        "progress_event",
+        {"message": "Finished step 1 of 3"},
+        config=config # Must be included for python < 3.10
+    )
+    await asyncio.sleep(1) # Placeholder for some slow operation
+    await adispatch_custom_event(
+        "progress_event",
+        {"message": "Finished step 2 of 3"},
+        config=config # Must be included for python < 3.10
+    )
+    await asyncio.sleep(1) # Placeholder for some slow operation
+    return "Done"
+
+slow_thing = RunnableLambda(slow_thing)
+
+async for event in slow_thing.astream_events("some_input", version="v2"):
+    print(event)
